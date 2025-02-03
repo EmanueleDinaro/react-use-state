7f8a51a4200253c9af090afefd71abe293ecc76a
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-03 03:14:11 +0100

    feat: hook useState and close all accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 export default function Accordion({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="accordion-card">
       <div className="accordion-name">
         <h3 className="title">{title}</h3>
         <button type="button">+</button>
       </div>
-      <div className="accordion-info">
-        <p>{description}</p>
-      </div>
+
+      {isOpen && (
+        <div className="accordion-info">
+          <p>{description}</p>
+        </div>
+      )}
     </div>
   );
 }
